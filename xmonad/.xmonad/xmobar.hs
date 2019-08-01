6527f6a2cd01a88687e370c7a0e0684e57219068
Max Berndt <max.berndt@iteratec.de>
2019-08-01 14:18:40 +0200

    initial commit

---
+++ xmonad/.xmonad/xmobar.hs
@@ -0,0 +1,20 @@
+Config { font = "xft:inconsolata:size=10:antialias=true"
+       , bgColor = "#002b36"
+       , fgColor = "#657b83"
+       , position = Top
+       , lowerOnStart = True
+       , commands = [ 
+		   -- Run Network "wlp2s0" ["-L","0","-H","32","--normal","green","--high","red"] 10
+                      Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
+                    , Run Memory ["-t","Mem: <usedratio>%"] 10
+                    , Run Date "<fc=#93a1a1>%a %b %_d %Y %H:%M</fc>" "date" 10
+                    , Run DiskIO [("/", "IO: <total>"), ("sda4", "<total>")] [] 10
+                    , Run StdinReader
+                    , Run Battery ["-t" , "Batt: <acstatus> (<left>%)", "-L","0","-H","32"] 50
+                    , Run Mpris2 "spotify" ["-t", "<artist> - [<composer>] <title>"] 10
+                    ]
+       , sepChar = "%"
+       , pickBroadest = False 
+       , alignSep = "}{"
+       , template = "%StdinReader% }{ %battery% %cpu% %memory% %diskio% %date%"
+       }
