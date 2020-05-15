f27e27d6ce77c3b690219b29a4dafd35196d2e5c
Max Berndt <mb@maxberndt.com>
2020-05-15 13:37:23 +0200

    multiCpu in xmobar

--- xmonad/.xmonad/xmobar.hs
+++ xmonad/.xmonad/xmobar.hs
@@ -5,7 +5,7 @@ Config { font = "xft:DejaVu Sans Mono for Powerline:size=9"
        , lowerOnStart = True
        , commands = [ 
 		   -- Run Network "wlp2s0" ["-L","0","-H","32","--normal","green","--high","red"] 10
-                      Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
+                      Run MultiCpu ["-t","Cpu: <total0>%|<total1>%|<total2>%|<total3>%","-L","50","-H","85","--normal","green","--high","red"] 10
                     , Run Memory ["-t","Mem: <usedratio>%"] 10
                     , Run Date "<fc=#93a1a1>%a %b %_d %Y %H:%M</fc>" "date" 10
                     , Run DiskIO [("/", "IO: <total>"), ("sda4", "<total>")] [] 10
@@ -16,5 +16,5 @@ Config { font = "xft:DejaVu Sans Mono for Powerline:size=9"
        , sepChar = "%"
        , pickBroadest = False 
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %battery% %cpu% %memory% %diskio% %date%"
+       , template = "%StdinReader% }{ %battery% %multicpu% %memory% %diskio% %date%"
        }
